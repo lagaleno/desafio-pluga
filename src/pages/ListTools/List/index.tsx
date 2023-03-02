03d1e3690585a154ce1556a33acc345bb54ccd3e
Larissa Galeno <galenolarissa@gmail.com>
2023-03-02 19:35:54 -0300

    refactor on pagination

--- src/pages/ListTools/List/index.tsx
+++ src/pages/ListTools/List/index.tsx
@@ -18,22 +18,21 @@ interface Response {
   data: ITool[];
 }
 
-// TODO: Refactor
-function loadItems(startCursor = 0, items: ITool[], total: number): Promise<Response> {
+// CONSTANTS
+const RESPONSE_TIME_IN_MS = 1000; // delay time to render next page
+const PAGE_MAX_SIZE = 12;
+
+function loadTools(startCursor = 0, items: ITool[], total: number): Promise<Response> {
   return new Promise((resolve) => {
     let newArray: ITool[] = [];
     let hasNextPage = false;
-    // TODO: magic number
-    const RESPONSE_TIME_IN_MS = 1000;
-    console.log("start cursor", startCursor, newArray);
     setTimeout(() => {
       if (startCursor < total) {
-        let missing = total - startCursor; 
-        // TODO: magic number
-        let ARRAY_SIZE = missing > 12 ? 12 : missing;
-        console.log("MISSING", missing, ARRAY_SIZE);
-        for (let i = startCursor; i < startCursor + ARRAY_SIZE; i++) {
-          const newItem = items[i] && items[i];
+        let missingElements = total - startCursor; 
+        let newElemetnsSize = missingElements > PAGE_MAX_SIZE ? PAGE_MAX_SIZE : missingElements;
+
+        for (let i = startCursor; i < startCursor + newElemetnsSize; i++) {
+          const newItem = items[i];
           newArray = [...newArray, newItem];
         }
 
@@ -44,20 +43,19 @@ function loadItems(startCursor = 0, items: ITool[], total: number): Promise<Resp
   });
 }
 
-export function useLoadItems(tools: ITool[]) {
+export function useLoadTools(tools: ITool[]) {
   const [loading, setLoading] = useState(false);
-  const [toolsP, setItems] = useState<ITool[]>([]);
+  const [currentTools, setCurrentTools] = useState<ITool[]>([]);
   const [hasNextPage, setHasNextPage] = useState<boolean>(true);
   const [error, setError] = useState<any>();
 
   async function loadMore() {
-    console.log("oi", toolsP);
     setLoading(true);
     try {
-      const { data, hasNextPage: newHasNextPage } = await loadItems(
-        toolsP.length, tools, tools.length
+      const { data, hasNextPage: newHasNextPage } = await loadTools(
+        currentTools.length, tools, tools.length
       );
-      setItems((current) => [...current, ...data]);
+      setCurrentTools((current) => [...current, ...data]);
       setHasNextPage(newHasNextPage);
     } catch (err) {
       setError(err);
@@ -66,11 +64,11 @@ export function useLoadItems(tools: ITool[]) {
     }
   }
 
-  return { loading, toolsP, hasNextPage, error, loadMore };
+  return { loading, currentTools, hasNextPage, error, loadMore };
 }
 
 const List = ({ tools }: IProps): JSX.Element => {
-  const { loading, toolsP, hasNextPage, error, loadMore } = useLoadItems(tools);
+  const { loading, currentTools, hasNextPage, error, loadMore } = useLoadTools(tools);
 
   const [sentryRef] = useInfiniteScroll({
     loading,
@@ -79,13 +77,13 @@ const List = ({ tools }: IProps): JSX.Element => {
     disabled: !!error,
     rootMargin: '0px 0px 400px 0px',
   });
-  console.log("toolsP", toolsP);
+
   return (
     <Box sx={{ m: 6 }}>
       <Grid container rowSpacing={6} columnSpacing={{ xs: 1, sm: 2, md: 6 }}>
-        {toolsP.map((tool) => (
+        {currentTools.map((tool) => (
           <Grid key={tool.app_id} item xs={12} sm={6} md={4}>
-            <ToolsCard icon={tool.icon} name={tool.name} /> 
+            <ToolsCard icon={tool.icon} name={tool.name}  color={tool.color} /> 
           </Grid>
         ))}
         {(loading || hasNextPage) && (
