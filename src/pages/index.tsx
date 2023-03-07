b3315e7b7ca11add43ff17a8893a7a094811b534
Larissa Galeno <galenolarissa@gmail.com>
2023-03-07 19:18:29 -0300

    adds componenet to when none tools exists

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,13 @@ import ListTools from "./ListTools";
 import { getToolsData } from "@/utils/getToolsData";
 import { sortDataByName } from "@/utils/sortDataByName";
 import { ITool } from "@/interfaces/ITool";
+import { Box, Grid, Typography } from "@mui/material";
 
 interface IProp {
   tools: ITool[];
 }
 
-export async function getStaticProps() {
+export const getStaticProps = async () => {
   const rawTools = await getToolsData();
   const tools = sortDataByName(rawTools);
   return {
@@ -16,12 +17,47 @@ export async function getStaticProps() {
       tools,
     },
   };
-}
+};
+
+const renderUnsuccessfulRequest = (): JSX.Element => {
+  return (
+    <>
+      <Box
+        m={6}
+        sx={{ width: "100%" }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography variant="h4" align="center">
+              Não foi possível obter dados sobre as ferramentas
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" display="block" align="center">
+              Entre em contato com a{" "}
+              <a href="https://pluga.co/" target="_blank">
+                Pluga
+              </a>{" "}
+              ou tente novamente mais tarde.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
 
 export default function Home({ tools }: IProp) {
   return (
     <LayoutDefault>
-      <ListTools tools={tools} />
+      {tools?.length > 0 ? (
+        <ListTools tools={tools} />
+      ) : (
+        renderUnsuccessfulRequest()
+      )}
     </LayoutDefault>
   );
 }
