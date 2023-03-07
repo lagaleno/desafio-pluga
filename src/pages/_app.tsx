d20fdb875b5ba80e1c263f0954c9266c9306835a
Larissa Galeno <galenolarissa@gmail.com>
2023-03-07 19:44:51 -0300

    add favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,15 @@ import type { AppProps } from "next/app";
 
 // Context imports
 import { RecentlyViewedToolsProvider } from "@/context/RecentlyViewedToolsContext";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+        <title> Ferramentas </title>
+      </Head>
       <RecentlyViewedToolsProvider>
         <Component {...pageProps} />
       </RecentlyViewedToolsProvider>
