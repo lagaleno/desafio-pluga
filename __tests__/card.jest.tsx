8a7b3bd68bbfcc2a74ea931673914ab5bff19df8
Larissa Galeno <galenolarissa@gmail.com>
2023-03-07 19:04:55 -0300

    fix coding styles of tests

--- __tests__/card.jest.tsx
+++ __tests__/card.jest.tsx
@@ -1,19 +1,19 @@
 // Jest imports
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import 'intersection-observer';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import "intersection-observer";
 
 // Component import
-import ToolCard from '@/components/ToolCard';
+import ToolCard from "@/components/ToolCard";
 
 // Mock import
 import toolsMock from "../__mocks__/toolsMock.json";
 
-describe ('Tool Card', () => {
-    it ('Renders component correctly', () => {
-        toolsMock.forEach((tool) => {
-            const { container } = render (<ToolCard tool={tool} />)
-            expect(container).toMatchSnapshot()
-        })           
-    })
-})
+describe("Tool Card", () => {
+  it("Renders component correctly", () => {
+    toolsMock.forEach((tool) => {
+      const { container } = render(<ToolCard tool={tool} />);
+      expect(container).toMatchSnapshot();
+    });
+  });
+});
